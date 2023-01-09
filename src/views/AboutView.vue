6bd219098acf8b6dae0dde54b206715dadaa4db8
yalandr <52127093+yalandr@users.noreply.github.com>
2023-01-09 10:22:22 -0500

    Add files via upload

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="container">
+      <h2>About this project</h2>
+      <p class="section-text">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores molestias, mollitia laudantium a reiciendis architecto perspiciatis? Tempora hic illo tempore libero. Vitae repudiandae eaque alias perferendis, architecto eveniet voluptates provident labore cum porro eligendi nostrum accusantium impedit nobis maiores similique, laboriosam ipsa eos fugit? Alias delectus voluptates incidunt esse vel?
+      </p>
+    </div>
   </div>
 </template>
